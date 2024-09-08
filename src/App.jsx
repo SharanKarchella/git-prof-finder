118466aacf1698b33c7932f2f9e5e2cebf1c982e
SharanKarchella <sharankarchella@gmail.com>
2024-09-08 18:53:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import GithubProfileFinder from "./index.jsx";
+import User from "./user.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <GithubProfileFinder />
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import User from "./user";
+import './styles.css';
+
+export default function GithubProfileFinder() {
+  const [userName, setUserName] = useState("SharanKarchella");
+  const [userData, setUserData] = useState(null);
+
+  function handleSubmit(){
+    fetchUserData()
+  }
+
+  async function fetchUserData() {
+    const response = await fetch(`https://api.github.com/users/${userName}`);
+    const res = await response.json();
+    if (res) {
+      setUserData(res);
+      setUserName("");
+    }
+  }
+
+  useEffect(() => {
+    fetchUserData();
+  }, ["res"]);
+
+  return (
+    <div className="container">
+      <div className="input-wrapper">
+        <input
+          type="text"
+          name="search-userName"
+          placeholder="Search Github Username..."
+          value={userName}
+          onChange={(evt) => setUserName(evt.target.value)}
+        />
+        <button onClick={handleSubmit}>Search</button>
+      </div>
+      {userData !== null ? <User user={userData} /> : null}
+    </div>
+  );
+}
+
+// async function fetchUserData() {
+//     const resp = await fetch(`https://api.github.com/users/${username}`);
+
+//     const data = resp.json();
+//     console.log(data);
+//   }
